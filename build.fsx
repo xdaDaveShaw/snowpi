9f01a62b660d34e7e5c2ea4bdccbfc665ea58d2d
Dave Shaw <dave@taeguk.co.uk>
2020-11-02 21:43:20 +0000

    Fake and Build basic

--- build.fsx
+++ build.fsx
@@ -1,16 +1,29 @@
 #r "paket:
-nuget Fake.Core.Target prerelease"
+nuget FSharp.Core 4.7.0
+nuget Fake.Core.Target //
+nuget Fake.DotNet.Cli //
+nuget Fake.DotNet.Paket //"
 #load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
+open Fake.DotNet
 
 // *** Define Targets ***
 Target.create "Clean" (fun _ ->
   Trace.log " --- Cleaning stuff --- "
 )
 
+Target.create "Paket Restore" (fun _ ->
+  
+  Trace.log " --- Paket Restore --- "
+
+  Paket.restore id
+)
+
 Target.create "Build" (fun _ ->
   Trace.log " --- Building the app --- "
+
+  DotNet.build id "snowpi.fsproj"
 )
 
 Target.create "Deploy" (fun _ ->
@@ -21,8 +34,9 @@ open Fake.Core.TargetOperators
 
 // *** Define Dependencies ***
 "Clean"
+  ==> "Paket Restore"
   ==> "Build"
   ==> "Deploy"
 
 // *** Start Build ***
-Target.runOrDefault "Deploy"
+Target.runOrDefault "Build"
