39ce5c5c5507c3d6607989a2ab272fa9b9999788
Dave Shaw <dave@taeguk.co.uk>
2020-11-04 21:04:03 +0000

    Basic working app

--- Program.fs
+++ Program.fs
@@ -1,59 +1,33 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open System.Drawing
+
+open rpi_ws281x
 
 [<EntryPoint>]
 let main argv =
-    Colorful.Console.WriteLine("Starting SnowPi!", Color.Blue)
 
-    let ledCount = 12
-    let pin = 18
-    let freq = 800000u
-    let dma = 10
-    let brightness = 100uy
-    let invert = false
-    let channel = 0
+    let settings = Settings.CreateDefaultSettings();
 
-    use strip = new ws281x.Net.Neopixel(ledCount, pin, frequency = freq, dma = dma, brightness = brightness, invert = invert, channel = channel)
-    (*
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms / 1000.0) *)
+    let controller = settings.AddController(ControllerType.PWM0, 12, StripType.Unknown, 100uy, false)
 
-    strip.Begin()
+    use rpi = new WS281x(settings)
+    
+    Console.WriteLine("Running...");
 
-    let np = strip.GetNumberOfPixels()
-    printfn "Number of pixels %d" np
+    controller.Reset();
+    controller.SetAll(Color.Red);
+    rpi.Render();
 
-    let wipe col = 
-        for i in 0..strip.GetNumberOfPixels() do
-            strip.SetPixelColor(i, col)
-            strip.Show()
-            Threading.Thread.Sleep(50)
+    Console.WriteLine("Render 1...");
+    Console.ReadLine() |> ignore;
+    
+    rpi.Reset();
 
-    //let rec mainLoop() = 
+    Console.WriteLine("Reset 1...");
+    Console.ReadLine() |> ignore;
 
-    Colorful.Console.WriteLine("Running, press Q to quit!", Color.Blue)
+    rpi.Render();
+    Console.WriteLine("Render 2...");
+    Console.ReadLine() |> ignore;
 
-    wipe Color.Red
-    wipe Color.Blue
-    wipe Color.Green
-
-        // if Console.KeyAvailable then
-        //     match Console.ReadKey().Key with
-        //     | ConsoleKey.Q -> ()
-        //     | _ -> mainLoop()
-        // else
-        //     mainLoop()
-
-
-    //mainLoop()
-
-    Colorful.Console.WriteLine("Press any key to end!", Color.Blue)
-
-    Console.ReadKey |> ignore
-
-    Colorful.Console.WriteLine("Ending SnowPi!", Color.Blue)
-    0 // return an integer exit code
+    0
