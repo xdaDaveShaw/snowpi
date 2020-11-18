b4248d279356903b33c1c629e09c2d5dfde47a30
Dave Shaw <dave@taeguk.co.uk>
2020-11-18 20:16:38 +0000

    Basic model to set LED's on Mock

--- Mock.fs
+++ Mock.fs
@@ -3,7 +3,70 @@ module Mock
 open Colorful
 open System.Drawing
 
-let run (c : Color) = 
+type Position =
+| BottomLeft
+| MiddleLeft
+| TopLeft
+| BottomRight
+| MiddleRight
+| TopRight
+| Nose
+| LeftEye
+| RightEye
+| BottomMiddle
+| MiddleMiddle
+| TopMiddle
+
+type State = 
+| Off
+| On of Color
+
+type LED = {
+    Position: Position
+    State : State
+}
+
+let posToLedNumber = function
+    | BottomLeft -> 0
+    | MiddleLeft -> 1
+    | TopLeft -> 2
+    | BottomRight -> 3
+    | MiddleRight -> 4
+    | TopRight -> 5
+    | Nose -> 6
+    | LeftEye -> 7
+    | RightEye -> 8
+    | BottomMiddle -> 9
+    | MiddleMiddle -> 10
+    | TopMiddle -> 11
+
+let run (leds : LED list) = 
+
+    let toFormat led = 
+        match led.State with
+        | On c -> Formatter("X", c)
+        | Off -> Formatter(" ", Color.Black)
+
+    let getOnLed i off =
+        let setLed = 
+            leds
+            |> List.choose (fun led -> 
+                if (posToLedNumber led.Position) = i then
+                    Some (toFormat led)
+                else
+                    None)
+            |> List.tryExactlyOne
+        match setLed with
+        | Some led -> led
+        | None -> off
+
+    let format =
+        Formatter(" ", Color.Black)
+        |> List.replicate 12
+        |> List.mapi getOnLed
+        |> List.toArray
+
+
     Console.WriteLineFormatted("""
     
     ###############
@@ -12,17 +75,17 @@ let run (c : Color) =
        #########
    #################
      /           \
-    /  [{0}]   [{0}]  \
+    /  [{7}]   [{8}]  \
    |               |
-    \     [{0}]     /
+    \     [{6}]     /
      \           /
      /           \
-    /     [{0}]     \
-   / [{0}]       [{0}] \
-  /       [{0}]       \
- |  [{0}]         [{0}]  |
-  \       [{0}]       /
-   \[{0}]         [{0}]/
+    /     [{11}]     \
+   / [{2}]       [{5}] \
+  /       [{10}]       \
+ |  [{1}]         [{4}]  |
+  \       [{9}]       /
+   \[{0}]         [{3}]/
     \_____________/
 
-    """, c, Color.White, "X")
+    """, Color.White, format)
--- Program.fs
+++ Program.fs
@@ -1,8 +1,23 @@
 ﻿open System.Drawing
 
+open Mock
+
 [<EntryPoint>]
 let main argv =
 
-    Mock.run Color.Red
+    let redNose = 
+        { Position = Nose 
+          State = On Color.Red }
+    let greenEyeL = 
+        { Position = LeftEye
+          State = On Color.LimeGreen }
+    let greenEyeR = 
+        { Position = RightEye
+          State = On Color.LimeGreen }
+    let topMiddle = 
+        { Position = TopMiddle
+          State = Off }
+
+    Mock.run [redNose; greenEyeL; greenEyeR; topMiddle]
 
     0
