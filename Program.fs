fa947fb43b58e03558ce50d49f435308acb1cf9a
Dave Shaw <dave@taeguk.co.uk>
2020-11-18 19:06:53 +0000

    Move real impl code into module

--- Program.fs
+++ Program.fs
@@ -1,33 +1,7 @@
-﻿open System
-open System.Drawing
-
-open rpi_ws281x
-
+﻿
 [<EntryPoint>]
 let main argv =
 
-    let settings = Settings.CreateDefaultSettings();
-
-    let controller = settings.AddController(ControllerType.PWM0, 12, StripType.Unknown, 100uy, false)
-
-    use rpi = new WS281x(settings)
-    
-    Console.WriteLine("Running...");
-
-    controller.Reset();
-    controller.SetAll(Color.Red);
-    rpi.Render();
-
-    Console.WriteLine("Render 1...");
-    Console.ReadLine() |> ignore;
-    
-    rpi.Reset();
-
-    Console.WriteLine("Reset 1...");
-    Console.ReadLine() |> ignore;
-
-    rpi.Render();
-    Console.WriteLine("Render 2...");
-    Console.ReadLine() |> ignore;
+    Real.run()
 
     0
---
+++ Real.fs
@@ -0,0 +1,31 @@
+module Real
+
+open System
+open System.Drawing
+open rpi_ws281x
+
+
+let run() =
+    let settings = Settings.CreateDefaultSettings();
+
+    let controller = settings.AddController(ControllerType.PWM0, 12, StripType.Unknown, 100uy, false)
+
+    use rpi = new WS281x(settings)
+    
+    Console.WriteLine("Running...");
+
+    controller.Reset();
+    controller.SetAll(Color.Red);
+    rpi.Render();
+
+    Console.WriteLine("Render 1...");
+    Console.ReadLine() |> ignore;
+    
+    rpi.Reset();
+
+    Console.WriteLine("Reset 1...");
+    Console.ReadLine() |> ignore;
+
+    rpi.Render();
+    Console.WriteLine("Render 2...");
+    Console.ReadLine() |> ignore;
